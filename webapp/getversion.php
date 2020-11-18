15ba04c6dd434e385e8c4d55c4ed757165590825
AndoroidX <andriytretyakov@gmail.com>
2020-11-18 18:51:12 +0200

    Some minor adds
    Added signing key
    Added csv backup

--- webapp/getversion.php
+++ webapp/getversion.php
@@ -4,7 +4,7 @@ $username = "USER_NAME";
 $password = "PASSWORD";
 $dbname = "DB_NAME";
 // Create connection
- $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
      	 die("Connection failed: " . $conn->connect_error);
